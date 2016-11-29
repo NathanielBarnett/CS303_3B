fdd9b09794671f995cc4ce30dfdc934d7090bb99
Nathaniel A. Barnett <thegrtn@gmail.com>
2016-11-29 17:14:57 -0600

    Updated, and functional

--- Morse_Tree.cpp
+++ Morse_Tree.cpp
@@ -34,28 +34,89 @@ void MorseTree::buildBinaryTree(const string& letter, const string& code)
 	//Follow through the data
 	for (unsigned int i = 0; i < code.length(); i++) //if I don't use unsigned I get an error
 	{
-		if (dot.find(code[i])) //if its dot, go left
+		if (code[i] == dot) //if its dot, go left
 		{
-			if (dataNode->right == nullptr) //Check to see if the node exists or not
+			if (dataNode->left == nullptr) //Check to see if the node exists or not
 			{
-				dataNode->right = new BTNode<string>("dummy node"); //If it does not exist, create a dummy
+				dataNode->left = new BTNode<string>("dummy node"); //If it does not exist, create a dummy
 			}
 			
-			dataNode = dataNode->right;
+			dataNode = dataNode->left;
 		}
 		
-		else if (dash.find(code[i])) //of its dash go right
+		else if (code[i] == dash) //of its dash go right
 		{
-			if (dataNode->left == nullptr)
+			if (dataNode->right == nullptr)
 			{
-				dataNode->left = new BTNode<string>("dummy node");
+				dataNode->right = new BTNode<string>("dummy node");
 			}
-			dataNode = dataNode->left;
+			dataNode = dataNode->right;
 		}
 	}
 
 	dataNode->data = letter; //since the node has to be filled with only the letter and not its data
 	return;
+}
+
+void MorseTree::fill_map()
+{
+	letter_morse['a'] = "._";
+	letter_morse['b'] = "_...";
+	letter_morse['c'] = "_._.";
+	letter_morse['d'] = "_..";
+	letter_morse['e'] = ".";
+	letter_morse['f'] = ".._.";
+	letter_morse['g'] = "__.";
+	letter_morse['h'] = "....";
+	letter_morse['i'] = "..";
+	letter_morse['j'] = ".___";
+	letter_morse['k'] = "_._";
+	letter_morse['l'] = "._..";
+	letter_morse['m'] = "__";
+	letter_morse['n'] = "_.";
+	letter_morse['o'] = "___";
+	letter_morse['p'] = ".__.";
+	letter_morse['q'] = "__._";
+	letter_morse['r'] = "._.";
+	letter_morse['s'] = "...";
+	letter_morse['t'] = "_";
+	letter_morse['u'] = ".._";
+	letter_morse['v'] = "..._";
+	letter_morse['w'] = ".__";
+	letter_morse['x'] = "_.._";
+	letter_morse['y'] = "_.__";
+	letter_morse['z'] = "__..";
+}
+
+void MorseTree::encode(string letters)
+{
+	while (letters.length() != 0)
+	{
+		cout << letter_morse.find(letters[0])->second << " ";
+		letters = letters.substr(1);
+	}
+}
+void MorseTree::decode(string code)
+{
+	dataNode = root;
+
+	//Follow through the data
+	for (unsigned int i = 0; i < code.length(); i++) //if I don't use unsigned I get an error
+	{
+		if (code[i] == dot ) //if its dot, go left
+		{
+			dataNode = dataNode->left;
+		}
+		else if (code[i] == dash) //if its dash go right
+		{
+			dataNode = dataNode->right;
+		}
+		else //Its a space
+		{
+			cout << dataNode->data << " "; //Print the letter
+			dataNode = root;
+		}
+	}
 }
 
 void MorseTree::print()
