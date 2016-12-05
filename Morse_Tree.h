514b651bc8f74055de52ab25582bfa8fa41f8a98
Nathaniel A. Barnett <thegrtn@gmail.com>
2016-12-04 21:26:59 -0600

    Commented, and updated...
    Overloaded the fill map function to work so that we can fill the map directly from the file...
    Don't have to use, but I thought I'd just add it so that it's there.

--- Morse_Tree.h
+++ Morse_Tree.h
@@ -1,4 +1,7 @@
 #pragma once
+//FANCY HEADER
+//FANCY HEADER
+//FANCY HEADER
 
 #include <iostream>
 #include <string>
@@ -13,15 +16,28 @@ class MorseTree
 {
 public:
 
+	//
 	void print();
+
+	//constructor
 	MorseTree() { ; }
 
+	//function to read in input from a pre-specified file in local directory, and populate tree with data from file
 	void filereading();
 
-	void buildBinaryTree(const string& readline, const string& code);
+	//takes in a string letter and a string code, and builds them into the Morse Tree
+	void buildBinaryTree(const string& letter, const string& code);
 
+	//using standard Morse encoding for alphabet
 	void fill_map();
+
+	//takes in a letter and the associated code and adds them into the map
+	void fill_map(const string& letter, const string& code);
+
+	//takes in a string of letters without spaces, and outputs the corresponding code seperated by spaces
 	void encode(string letters);
+
+	//takes in a string of dots and dashes with a space inbetween words, and outputs the corresponding letters
 	void decode(string code);
 
 private:
@@ -29,8 +45,12 @@ private:
 	char dot = '.';
 	char dash = '_';
 
+	//Node used to trace through tree
 	BTNode<string> *dataNode = new BTNode<string>("root");
+
+	//Node used as root of Morse Tree
 	BTNode<string> *root = dataNode;
 
+	//map to store values for encoding of strings in Morse
 	map<char, string> letter_morse;
 };
